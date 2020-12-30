2dc7c4134fab3d2b3c79cd6473d0d00419e70801
Thej <tkodiyat01@gmail.com>
2020-12-30 17:46:14 -0500

    add urls connection from api to product apps urls.py Removed DB info from github

--- ecom/api/urls.py
+++ ecom/api/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 from rest_framework.authtoken import views
 from .views import home
 
+
 urlpatterns = [
     path('', home, name='api.home'),
-    path('category/', include('api.category.urls'))
+    path('category/', include('api.category.urls')),
+    path('product/', include('api.product.urls'))
 ]

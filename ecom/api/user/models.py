879194879bf2c949cf8e5470af038445897405f4
Thej <tkodiyat01@gmail.com>
2020-12-30 19:41:12 -0500

    created a custom User model by modifying AbstractUser

--- ecom/api/user/models.py
+++ ecom/api/user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+# creating a new user model by inheriting AbstractUser model and changing username to email
+# Also updating a few extra fields like phone, gender and session token
+class CustomUser(AbstractUser):
+    name = models.charField(max_length=50, default='Anonymous')
+    email = models.EmailField(max_length=254, unique=True)
+
+    username = None
+    # username will be governed by email
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    phone = CharField(max_length=20 blank=True. null=True)
+    gender = CharField(max_length=10 blank=True. null=True)
+
+    session_token = models.CharField(max_length=10, default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

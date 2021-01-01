b4f039b7ef6220e2b2e61053cf858fabcd047bf0
Thej <tkodiyat01@gmail.com>
2020-12-31 20:47:16 -0500

    Added signout method and permissions class

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -67,3 +67,35 @@ def signin(request):
 
     except UserModel.DoesNotExist:
         return JsonResponse({'error': 'Invalid Email'})
+
+def signout(request, id):
+    # Django built in logout method
+    logout(request)
+
+    # Gets/Prepares all the user models data
+    UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(pk=id)
+        user.session_token = "0"
+        user.save()
+    
+    except UserModel.DoesNotExist:
+        return JsonResponse({'error': 'Invalid User ID'})
+    
+    return JsonResponse({'success': 'Logout sucess'})
+
+# This allow us to give full permission to all users. Can be customized for SuperUser etc
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes_by_action = {'create': [AllowAny]}
+
+    queryset = CustomUser.objects.all().order_by('id')
+    serializers_class = UserSerializer
+
+    def get_permissions(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            return [permission() for permission in self.permission_classes_by_action]
+
+

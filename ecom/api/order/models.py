4924cee8dbd4a27b224e44e5e4e03c8326139474
Thej <tkodiyat01@gmail.com>
2021-01-01 10:40:33 -0500

    added model for order app

--- ecom/api/order/models.py
+++ ecom/api/order/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from api.user.models import CustomUser
+from api.product.models import Product
 
 # Create your models here.
+
+class Order(model.Models):
+    # user will be pick up from the CustomUser model, connected through the ForeignKey
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
+
+    product_names = models.CharField(max_length=500)
+    total_products = models.CharField(max_length=500, default=0)
+    transcation_id = models.CharField(max_length=150, default=0)
+    total_amount = models.CharField(max_length=50, default=0)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

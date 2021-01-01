817e3150eff7324aa4ded749412c49ae650b28ff
Thej <tkodiyat01@gmail.com>
2020-12-31 23:52:00 -0500

    registered the CustomUser Model in djang admin rest_framework

--- ecom/api/user/admin.py
+++ ecom/api/user/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import CustomUser
 # Register your models here.
 
-# admin.site.register(CustomUser)
+# This makes sure the rest_framework can access the CustomUser DB
+admin.site.register(CustomUser)

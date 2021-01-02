606f7a03988c50744eb17ed4ee2ff3f002ef3f36
Thej <tkodiyat01@gmail.com>
2021-01-01 21:04:34 -0500

    created the OrderViewSet class, add method to add order

--- ecom/api/category/views.py
+++ ecom/api/category/views.py
@@ -4,6 +4,7 @@ from .models import Category
 
 # Create your views here.
 
+# This by default gives you all the necessary CRUD operations for Category model through Django libs
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all().order_by('name')
     serializer_class = CategorySerializers
--- ecom/api/order/views.py
+++ ecom/api/order/views.py
@@ -19,3 +19,34 @@ def validate_user_session(id, token):
         return False
     except UserModel.DoesNotExist:
         return False
+
+@csrf_exempt
+def add(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Please login first', 'code': '400'})
+
+    if request.method == 'POST':
+        user_id = id
+        transcation_id = request.POST['transaction_id']
+        amount = request.POST['amount']
+        products = request.POST['prouducts']
+        
+        # to get the total products, we split the product list by ',', get the length of the return array and reduce by 1
+        total_product = len(products.split(',')[:-1])
+
+        # Getting the user details
+        UserModel = get_user_model()
+        try:
+            user = UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return JsonResponse({'error': 'User does not exist'})
+        
+        # Creating and saving a new order to the DB
+        order = Order(user=user, product_name=products, total_product=total_product, transcation_id=transcation_id, total_amount=amount)
+        order.save()
+        return JsonResponse({'success': True, 'error': False, 'msg': 'Order Placed Successfully'})
+
+# This by default gives all the CRUD capabilities for Order model
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all().order_by('id')
+    serializer_class = OrderSerializers

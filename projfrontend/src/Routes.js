98f74b087b64ff5b4b909294719c7145a9bae5a2
Thej <tkodiyat01@gmail.com>
2021-01-25 17:36:25 -0500

    Create basic signin component and set the route for it

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -2,8 +2,11 @@ import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import PrivateRoutes from './auth/helper/PrivateRoutes'
 import Home from './core/Home'
+import Signin from './user/Signin'
 import Signup from './user/Signup'
 import UserDashboard from './user/UserDashboard'
+
+
 
 // ES 6 syntax - same as the 'export default function Routes...'
 const Routes = () => {
@@ -12,6 +15,7 @@ const Routes = () => {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/signup" exact component={Signup} />
+                <Route path="/signin" exact component={Signin} />
                 <PrivateRoutes path="/user/dashboard" exact component={UserDashboard}/>
             </Switch>
         </BrowserRouter>
---
+++ projfrontend/src/user/Signin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Base from '../core/Base'
+
+
+const Signin = () => {
+    return (
+        <Base title="Signin Page" description="T-shirt store">
+        <p>Welcome to Signin Page</p>
+        </Base>
+    )
+}
+
+export default Signin

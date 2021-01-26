1cd02160b1d91d5a2a9cad1cc845d326e30da597
Thej <tkodiyat01@gmail.com>
2021-01-26 13:28:20 -0500

    Loaded all the products to cart component Used setState get the products and maintaining it Used useEffect to load the data before the component is shown

--- projfrontend/src/core/Cart.js
+++ projfrontend/src/core/Cart.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
 import Base from './Base'
+import Card from './Card'
+import { loadCart } from './helper/cartHelper'
+
 
 
 const Cart = () => {
+    
+    const [products, setProducts] = useState([])
+    // useEffect is used when you want something load before component is shown
+    useEffect(() => {
+        // loadCart -returns the value of 'cart' from window session
+        // setProducts will set that value (object) to products variable in this component
+        setProducts(loadCart())
+    }, [])
+
+    const loadAllProducts = (products) => {
+        return (
+            <div>
+                {/* If i use {} then i need to use the return key. OR we need to use the (). Look carefully at the brackets used below */}
+                {products.map((product, index) => (
+                    <Card
+                    key={index}
+                    product={product}
+                    removeFromCart={true}
+                    addtoCart={false}
+                    />
+                ))}
+            </div>
+        )
+    }
+
+    const loadCheckout = () => {
+        return (
+            <h1>Checkout</h1>
+        )
+    }
+
     return (
-       <Base title="Cart Page" description="A basic Cart Page">
-           <h1>Cart Page</h1>
+       <Base title="Cart Page" description="Welcome to Checkout">
+           <div className="row text-centre">
+               <div className="col-6">
+                   {loadAllProducts(products)}
+               </div>
+               <div className="col-6">
+                   {loadCheckout()}
+               </div>
+           </div>
        </Base>
     )
 }

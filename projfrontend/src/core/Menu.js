3be229fc8b6843da4bce1fac1ee360aa494feb6b
Thej <tkodiyat01@gmail.com>
2021-01-26 11:39:36 -0500

    added withRouter, Link to navbar

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -1,18 +1,29 @@
 import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
 
-const Menu = () => {
+const currentTab = (history, path) => {
+    // If you are in the home page then 'Home' in navbar will be highlighted with '#2ecc72' color
+    if (history.location.pathname === path) {
+        return {color: '#2ecc72'}
+    } else {
+        return {color: '#FFFFFF'}
+    }
+}
+
+const Menu = ({history, path}) => {
     return (
         <div>
             <ul className="nav nav-tabs bg-dark">
                 <li className="nav-item">
-                    <a className="nav-link" href="#">Home</a>
+                    <Link style={currentTab(history, "/")} className="nav-link" to="/">Home</Link>
                 </li>
                 <li className="nav-item">
-                    Signin
+                    <Link style={currentTab(history, "/signin")} className="nav-link" to="/signin">Signin</Link>
                 </li>
             </ul>
         </div>
     )
 }
 
-export default Menu
+// Allows you keep a history of users paths
+export default withRouter(Menu)
